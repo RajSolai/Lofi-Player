6cdbc1d66555b68e78910fa1f74969a6773b4039
Solai Raj <msraj085@gmail.com>
2021-05-22 20:13:30 +0530

    added the splash view

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,54 @@
 import 'dart:ui';
 
+import 'package:flutter/services.dart';
 import 'package:video_player/video_player.dart';
 import 'package:flutter/material.dart';
 import 'package:toast/toast.dart';
 
-void main() => runApp(HomePage());
+void main() => runApp(App());
 
-class HomePage extends StatefulWidget {
+class App extends StatefulWidget {
   @override
-  _HomePage createState() => _HomePage();
+  _App createState() => _App();
 }
 
-class _HomePage extends State<HomePage> {
+class _App extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: SplashView());
+  }
+}
+
+class HomeView extends StatefulWidget {
+  HomeView({this.songs});
+  final List<Map> songs;
+  @override
+  _HomeView createState() => _HomeView();
+}
+
+class _HomeView extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(children: [
+      PageView(
+          scrollDirection: Axis.vertical,
+          children: widget.songs
+              .map((song) => VideoView(
+                    videourl: song['uri'],
+                    songname: song['title'],
+                  ))
+              .toList())
+    ]);
+  }
+}
+
+class SplashView extends StatefulWidget {
+  @override
+  _SplashView createState() => _SplashView();
+}
+
+class _SplashView extends State<SplashView> {
+  bool _isLoading = true;
   var songs = [
     {
       "title": "Jinsang- Affection",
@@ -24,23 +61,39 @@ class _HomePage extends State<HomePage> {
           "https://firebasestorage.googleapis.com/v0/b/pushy-9740f.appspot.com/o/vid2.mp4?alt=media&token=162b6b2e-4abf-4f83-88a1-e48bae40d966"
     }
   ];
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-          backgroundColor: Colors.black,
-          body: Stack(children: [
-            PageView(
-                scrollDirection: Axis.vertical,
-                children: songs
-                    .map((song) => VideoView(
-                          videourl: song['uri'],
-                          songname: song['title'],
-                        ))
-                    .toList())
-          ])),
-    );
+    return Scaffold(
+        backgroundColor: Colors.black,
+        body: _isLoading
+            ? Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      "LofiPlayer",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 32.0),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(40.0),
+                      child: CircularProgressIndicator(color: Colors.white),
+                    ),
+                    ElevatedButton(
+                      child: Text("Debug"),
+                      onPressed: () => setState(() {
+                        _isLoading = false;
+                      }),
+                    )
+                  ],
+                ),
+              )
+            : HomeView(
+                songs: songs,
+              ));
   }
 }
 
